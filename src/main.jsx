0beaef14edfbc34906480c74a907a6d1c3b2cf5f
John Weathers <weathers.johnb@gmail.com>
2023-07-01 13:55:36 -0500

    Preparation for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import { router, queryClient } from './App';
 import { AuthProvider } from './context/AuthProvider';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (import.meta.env.PROD) disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
