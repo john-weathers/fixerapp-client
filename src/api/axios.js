11f81d252e7dd202e635964b3c8626d0c690e444
John Weathers <weathers.johnb@gmail.com>
2023-07-02 13:49:46 -0500

    Preparation for deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://fixerapp-api.johnweathers.onrender.com';
+const BASE_URL = 'https://fixerapp-api.onrender.com';
 
 export default axios.create({
     baseURL: BASE_URL
